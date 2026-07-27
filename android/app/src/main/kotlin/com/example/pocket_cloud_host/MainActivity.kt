2aada63445b4f7b8ecb581ec966e456f98c900fe
Your Name <helppterooai@gmail.com>
2026-07-27 23:32:18 +0300

    Fix package name in MainActivity and move to correct path

--- android/app/src/main/kotlin/com/example/pocket_cloud_host/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.pocket_cloud_host
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/helpterooai/local_server/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.helpterooai.local_server
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
