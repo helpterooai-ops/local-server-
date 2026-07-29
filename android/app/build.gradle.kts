a69360060aa46bd83952c256c902fe4480e657b7
Your Name <helppterooai@gmail.com>
2026-07-29 19:20:31 +0300

    رفع compileSdk إلى 36 لإصلاح توافق file_picker

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.helpterooai.local_server"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
