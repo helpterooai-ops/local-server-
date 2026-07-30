5f084a8fca33ae7b7309ca5527c9abefb62cfab3
Your Name <helppterooai@gmail.com>
2026-07-30 08:37:56 +0300

    تنظيف المشروع من البقايا المؤقتة باستخدام flutter clean

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -7,8 +7,8 @@ buildscript {
     }
     dependencies {
         classpath("com.google.gms:google-services:4.4.2")
-        // تم تصحيح رقم الإصدار إلى النسخة المتوفرة والمستقرة
-        classpath("com.chaquo.python:gradle:14.0.2")
+        // أحدث إصدار مستقر، متوافق مع Gradle 9 و AGP 9.x
+        classpath("com.chaquo.python:gradle:17.0.0")
     }
 }
 
@@ -16,6 +16,8 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+        // نفس مستودع Chaquopy مطلوب هنا أيضًا لحل مكتباته وقت البناء
+        maven { url = uri("https://chaquo.com/maven") }
     }
 }
 
@@ -28,7 +30,7 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-    
+
     // إجبار جميع المكتبات على استخدام compileSdk 36
     afterEvaluate {
         extensions.findByType<com.android.build.gradle.BaseExtension>()?.let { ext ->
